eb90e24a148501cc3ca3ff3e4f46942c31eeebd1
Alicia <tl2180@nyu.edu>
2019-06-17 14:38:09 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+To install the dependencies, run: pip install -r requirements.txt.
+
+Run the code sample without specifying any arguments: python sample.py
+
+Run the code sample by specifying the optional arguments: python sample.py --term="bars" --location="San Francisco, CA"
---
+++ sample.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+"""
+Yelp Fusion API code sample.
+This program demonstrates the capability of the Yelp Fusion API
+by using the Search API to query for businesses by a search term and location,
+and the Business API to query additional information about the top result
+from the search query.
+Please refer to http://www.yelp.com/developers/v3/documentation for the API
+documentation.
+This program requires the Python requests library, which you can install via:
+`pip install -r requirements.txt`.
+Sample usage of the program:
+`python sample.py --term="bars" --location="San Francisco, CA"`
+"""
+from __future__ import print_function
+
+import argparse
+import json
+import pprint
+import requests
+import sys
+import urllib
+import time
+
+#import api key
+import config
+
+try:
+    # For Python 3.0 and later
+    from urllib.error import HTTPError
+    from urllib.parse import quote
+    from urllib.parse import urlencode
+except ImportError:
+    # Fall back to Python 2's urllib2 and urllib
+    from urllib2 import HTTPError
+    from urllib import quote
+    from urllib import urlencode
+
+
+# Yelp Fusion no longer uses OAuth as of December 7, 2017.
+# You no longer need to provide Client ID to fetch Data
+# It now uses private keys to authenticate requests (API Key)
+# You can find it on
+# https://www.yelp.com/developers/v3/manage_app
+API_KEY= config.api_key
+
+
+# API constants, you shouldn't have to change these.
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
+
+
+# Defaults 
+DEFAULT_TIME = time.strftime('%H:%M')  #get current time 
+DEFAULT_LONGITUDE = -73.99429321289062
+DEFAULT_LATITUDE = 40.70544486444615 
+DEFAULT_RADIUS = 1000 #meters
+SEARCH_LIMIT = 1
+
+
+def request(host, path, api_key, url_params=None):
+    """Given your API_KEY, send a GET request to the API.
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        API_KEY (str): Your API Key.
+        url_params (dict): An optional set of query parameters in the request.
+    Returns:
+        dict: The JSON response from the request.
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
+    url_params = url_params or {}
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    headers = {
+        'Authorization': 'Bearer %s' % api_key,
+    }
+
+    print(u'Querying {0} ...'.format(url))
+
+    response = requests.request('GET', url, headers=headers, params=url_params)
+
+    return response.json()
+
+
+def search(api_key, current_time, longitude,latitude, radius):
+    """Query the Search API by a search term and location.
+    Args:
+        term (str): The search term passed to the API.
+        location (str): The search location passed to the API.
+        longitude (decimal): decimal	Required if location is not provided. Longitude of the location you want to search nearby.
+        latitude (decimal): decimal	Required if location is not provided. Latitude of the location you want to search nearby.
+    Returns:
+        dict: The JSON response from the request.
+    """
+
+    #############################################
+    #decide term eg: breakfast, brunch, lunch, dinner
+    if current_time <= "10:30":
+        term = "breakfast brunch"
+    elif current_time <= "14:00":
+        term = "brunch lunch"
+    else:
+        term = "dinner"
+
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'longitude': longitude,
+        'latitude': latitude,
+        "radius": radius ,
+        'limit': SEARCH_LIMIT
+    }
+    return request(API_HOST, SEARCH_PATH, api_key, url_params=url_params)
+
+
+def get_business(api_key, business_id):
+    """Query the Business API by a business ID.
+    Args:
+        business_id (str): The ID of the business to query.
+    Returns:
+        dict: The JSON response from the request.
+    """
+    business_path = BUSINESS_PATH + business_id
+
+    return request(API_HOST, business_path, api_key)
+
+
+def query_api(current_time, longitude, latitude, radius):
+    """Queries the API by the input values from the user.
+    Args:
+        term (str): The search term to query.
+        location (str): The location of the business to query.
+        longitude (decimal): decimal	Required if location is not provided. Longitude of the location you want to search nearby.
+        latitude (decimal): decimal	Required if location is not provided. Latitude of the location you want to search nearby.
+    """
+    start = time.time()
+    print("-----start-----\n")
+
+    response = search(API_KEY, current_time, longitude, latitude, radius)
+
+    print(response)
+
+    end = time.time()
+    print("-----end-----\n")
+    print(end-start)
+
+    # businesses = response.get('businesses')
+
+    # if not businesses:
+    #     print(u'No businesses for {0} in [{1},{2}] found.'.format(term,longitude, latitude))
+    #     return
+
+    # business_id = businesses[0]['id']
+
+    # print(u'{0} businesses found, querying business info ' \
+    #     'for the top result "{1}" ...'.format(
+    #         len(businesses), business_id))
+    # response = get_business(API_KEY, business_id)
+
+    # print(u'Result for business "{0}" found:'.format(business_id))
+    # pprint.pprint(response, indent=2)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-t', '--time', dest='current_time', default=DEFAULT_TIME,
+                        type=str, help='Search term (default: %(default)s)')
+    parser.add_argument('-long', '--longitude', dest='longitude',
+                        default=DEFAULT_LONGITUDE, type=float,
+                        help='Search longtitude (default: %(default)s)')
+    parser.add_argument('-lat', '--latitude', dest='latitude',
+                        default=DEFAULT_LATITUDE, type=float,
+                        help='Search latitude (default: %(default)s)')
+    parser.add_argument('-r', '--radius', dest='radius',
+                        default=DEFAULT_RADIUS, type=float,
+                        help='Search radius (default: %(default)s)')
+
+    input_values = parser.parse_args()
+
+    try:
+        query_api(input_values.current_time, input_values.longitude, input_values.latitude, input_values.radius )
+    except HTTPError as error:
+        sys.exit(
+            'Encountered HTTP error {0} on {1}:\n {2}\nAbort program.'.format(
+                error.code,
+                error.url,
+                error.read(),
+            )
+        )
+
+
+if __name__ == '__main__':
+    main()
