c8faa7fc957c1a90cd373c8eb53e678859974c06
alicia <32725332+Alicia1529@users.noreply.github.com>
2019-08-09 22:27:19 -0400

    Delete config.py

--- main/config.py
+++
@@ -1,3 +0,0 @@
-api_key = "0IlhE-EcPD4RAWMP4wcNhqBXCwl6I6uzy_gDpt2zvNRtT0XP1nOOj5tBP0m4GmiVse0goc7-TRrKvhQv340XB-qTDnQ56p7X-yXekgyrVgFKeYNNucpFztOhTbf_XHYx"
-client_id = "v5s9e6q31PPTIr0IFP5wxQ"
-app_name = "Recommender system development and deployment for elderly mobility in NYC"
